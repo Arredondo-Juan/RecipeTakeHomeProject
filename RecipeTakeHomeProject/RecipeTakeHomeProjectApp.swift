51fb553102e09cfe0a091bcdff2a0e90bbfb30ca
Arredondo-Juan <juan_arredondo@icloud.com>
2024-11-13 22:02:20 -0600

    Initial Commit

---
+++ RecipeTakeHomeProject/RecipeTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTakeHomeProjectApp.swift
+//  RecipeTakeHomeProject
+//
+//  Created by Juan Arredondo on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
